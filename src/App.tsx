adeeab9a99f91fae1cf061839fee11f8fd39d8e5
SecondNiceee <col1596321@gmail.com>
2024-10-12 04:28:33 +0300

    base-setings

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import './App.css';
 
-function App() {
+import "./scss/_index.scss"
+const App = () => {
   return (
-    <>
-    </>
+    <div>
+      
+    </div>
   );
-}
+};
 
 export default App;
